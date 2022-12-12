d100d7479a7db90a2b3e3a9937753c242988810e
emi27 <emi273@gmail.com>
2022-12-12 23:45:50 +0200

    add preventDefoult

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import './App.css';
-import ReactDOM from 'react-dom';
 import UploadData from './components/UploadData'
-import ShapleyGraph from './components/ShapleyGraph'
+import React, { useState } from "react";
+// import ShapleyGraph from './components/ShapleyGraph'
 
 
 
@@ -12,8 +11,8 @@ function App() {
       <div className='test'>
         <UploadData />
       </div>
-      <ShapleyGraph data={[-100, 430, 448, 470, 540, 580, 690, 1100, 1200, 1380]}
-       attributes={['South Korea', 'Canada', 'United Kingdom', 'Netherlands', 'Italy', 'France', 'Japan','United States', 'China', 'Germany']} />
+      {/* <ShapleyGraph data={[-100, 430, 448, 470, 540, 580, 690, 1100, 1200, 1380]}
+       attributes={['South Korea', 'Canada', 'United Kingdom', 'Netherlands', 'Italy', 'France', 'Japan','United States', 'China', 'Germany']} /> */}
     </div>
   );
 }
